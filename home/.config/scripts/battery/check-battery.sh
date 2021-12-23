f54fd21b7ea54fd0a8a04dd7a2f9f9bd4133f79d
albamr09 <bertaalba1@gmail.com>
2021-12-23 20:05:28 +0100

    [scripts/dunst] Check battery fully charged/Ignore caffeine

--- home/.config/scripts/battery/check-battery.sh
+++ home/.config/scripts/battery/check-battery.sh
@@ -2,7 +2,10 @@
 
 BATTINFO=`acpi -b | sed  /"Unknown"/d`
 PRINTINFO=`echo $BATTINFO | cut --fields=3,4 -d " " | awk '{ print substr( $0, 1, length($0)-1 ) }'`
+PERCERTAGE=`echo $BATTINFO | cut -f 4 -d " " | awk '{ print substr( $0, 1, length($0)-2 ) }'`
 
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " " | awk '{ print substr( $0, 1, length($0)-2 ) }'` -lt 15 ]] ; then
+if [[ `echo $BATTINFO | grep Discharging` && $PERCERTAGE -lt 15 ]] ; then
        /usr/bin/notify-send -u critical "battery" "$PRINTINFO"
+elif [[ `echo $BATTINFO | grep Charging` && $PERCERTAGE -gt 95 ]] ; then
+       /usr/bin/notify-send -u normal "battery" "Fully charged!"
 fi
