d3b6a51b97c79d171d6012952d43c38282225d25
Alba Martínez Rodríguez <bertaalba1@gmail.com>
2022-09-17 16:04:18 +0200

    [nvim] Add debugpy package for debugging

--- install.sh
+++ install.sh
@@ -91,6 +91,7 @@ configurar_nvim(){
     wd=$(pwd)
     mkdir -p /home/$USER/.virtualenvs && cd /home/$USER/.virtualenvs
     python -m venv debugpy
+	/home/$USER/.virtualenvs/debugpy/bin/pip3 install debugpy
     cd $wd
 
 	curl -fLo /home/$USER/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
