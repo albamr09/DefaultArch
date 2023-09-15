827a8e1e883df32de2ba63930c1f376b02a32416
albamr09 <87711484+albamr09@users.noreply.github.com>
2023-09-15 16:27:30 +0200

    Update config.py

--- home/.config/qutebrowser/config.py
+++ home/.config/qutebrowser/config.py
@@ -1,4 +1,4 @@
 config.load_autoconfig()
-c.url.searchengines = {'DEFAULT': 'https://start.duckduckgo.com/search?q={}'}
-c.url.default_page = 'https://start.duckduckgo.com/'
-c.url.start_pages = ['https://start.duckduckgo.com/']
+c.url.searchengines = {'DEFAULT': 'https://duckduckgo.org/search?q={}'}
+c.url.default_page = 'https://duckduckgo.org/'
+c.url.start_pages = ['https://.duckduckgo.org/']
